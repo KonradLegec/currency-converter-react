a06c2f2c6ac5ff8992a0a3f0e3f6c4f170dc3ccb
Konrad Legec <konrad.legec@gmail.com>
2022-10-20 22:06:45 +0200

    added footer

---
+++ src/Footer/index.js
@@ -0,0 +1,13 @@
+import "./style.css";
+
+const Footer = () => (
+  <footer>
+    <div class="footer">
+      Kalkulator walut - wersja testowa - kursy walut aktualne na dzień
+      28/08/2022
+      <p>&copy; Konrad Legęć 2022</p>
+    </div>
+  </footer>
+);
+
+export default Footer;
